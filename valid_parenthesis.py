ab34e08a43000d42077c067caa9836156dd98454
Ria Thazhe Punathil <riachandrantp@gmail.com>
2022-03-27 17:22:52 -0400

    Create valid_parenthesis.py

---
+++ valid_parenthesis.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bracket_map = {"(": ")", "[": "]",  "{": "}"}
+        open_bracket = ["(", "[", "{"]
+        stack = []
+        for i in s:
+            if i in open_bracket:
+                stack.append(i)
+            elif stack and i == bracket_map[stack[-1]]:
+                    stack.pop()
+            else:
+                return False
+        return len(stack) == 0
