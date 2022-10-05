be12a7ee8c8778d6fadddec1a545408e37ce42b5
Ria Thazhe Punathil <riachandrantp@gmail.com>
2022-10-04 23:45:11 -0400

    Create binary_tree_traverse.py

---
+++ binary_tree_traverse.py
@@ -0,0 +1,19 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+        
+        def helper(node,level):
+            if len(levels) == level:
+                levels.append([])
+                
+            levels[level].append(node.val)
+            
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+                
+        helper(root,0)
+        return levels
