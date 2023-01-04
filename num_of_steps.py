62aa8eb61f255d2daf2963644fcb07e970079ee5
Ria Thazhe Punathil <riachandrantp@gmail.com>
2023-01-04 15:53:16 +0530

    Create num_of_steps.py

---
+++ num_of_steps.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        count = 0
+        while num != 0:
+            if num%2 == 0: num /= 2
+            elif num%2 == 1: num -= 1
+            count += 1
+        return count
