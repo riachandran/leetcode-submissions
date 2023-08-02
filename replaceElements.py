1f78486313e4a485f149021b36062d1b7cdeb98f
Ria Thazhe Punathil <riachandrantp@gmail.com>
2023-08-02 18:42:40 -0400

    Create replaceElements.py

---
+++ replaceElements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        size = len(arr)
+        max_from_right = arr[size-1]
+        
+        arr[size-1] = -1
+
+        for i in range(size-2,-1,-1):
+
+            temp = arr[i]
+
+            arr[i]=max_from_right
+
+            if max_from_right< temp:
+                max_from_right=temp
+            
+        return arr
