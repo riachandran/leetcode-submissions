a3fbc48b5c72660513beae1579e8a14ec8a4b6be
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-04-03 22:41:23 -0700

    Create dotProduct.py

---
+++ dotProduct.py
@@ -0,0 +1,17 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        self.vec = vec.nums
+        result = 0
+        for x,y in zip(self.nums,self.vec):
+            result += x*y
+        
+        return result
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
