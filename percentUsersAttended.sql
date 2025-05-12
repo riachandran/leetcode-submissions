80b3e36d15790f6160f17ee93ebfc1d713111125
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-05-12 15:47:10 -0700

    Create percentUsersAttended.sql

---
+++ percentUsersAttended.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT r.contest_id, ROUND(COUNT(r.user_id)*100/(SELECT COUNT(u.user_id) FROM Users u),2) as percentage FROM
+Register r GROUP BY contest_id ORDER BY percentage DESC, r.contest_id
