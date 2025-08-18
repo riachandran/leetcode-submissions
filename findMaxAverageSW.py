8c9601918a7504a922f83da06b639c117cc50940
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-08-18 14:33:34 -0700

    Create findMaxAverageSW.py

---
+++ findMaxAverageSW.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        curr = 0
+        for i in range(k):
+            curr += nums[i]
+            
+        ans = curr/k
+        for i in range(k,len(nums)):
+            curr += nums[i] - nums[i-k]
+            avg = curr/k
+            ans = max(ans,avg)
+            
+        return ans
+        
