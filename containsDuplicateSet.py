2ddd9bad741be93e9a6ce1efa1e09cff571c22f0
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-08-23 00:28:19 -0700

    Create containsDuplicateSet.py

---
+++ containsDuplicateSet.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = set()
+        for num in nums:
+            if num in dic: return True
+            else: dic.add(num)
+
+        return False
