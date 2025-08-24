654ae372301df20fbce8992d4487355334606aa1
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-08-24 01:16:59 -0700

    Create maxSubarrayLength.py

---
+++ maxSubarrayLength.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        freq = defaultdict(int)
+        ans = left = 0
+
+        for right in range(len(nums)):
+            freq[nums[right]] += 1
+            if freq[nums[right]] > k:
+                while nums[left] != nums[right]:
+                    freq[nums[left]] -= 1
+                    left += 1
+                freq[nums[left]] -= 1
+                left += 1
+
+            ans = max(ans,right-left+1)
+
+
+        return ans
