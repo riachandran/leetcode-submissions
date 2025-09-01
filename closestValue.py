9feb7ca9f3a72336f6dae4037c6d1ed60c41e9a0
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-09-01 14:51:42 -0700

    Create closestValue.py

---
+++ closestValue.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        ans = root.val
+        while root:
+            ans = min(ans,root.val,key=lambda x: (abs(target-x),x))
+            root = root.left if target < root.val else root.right
+            
+        return ans
