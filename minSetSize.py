937993aff6c8eac335ef92ed0855ea9be7e383bc
Ria Thazhe Punathil <riachandrantp@gmail.com>
2025-09-02 01:02:43 -0700

    Create minSetSize.py

---
+++ minSetSize.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        counter, half, length = 0, len(arr)//2, len(arr)
+        for x in sorted(Counter(arr).values(),reverse=True):
+            length -= x
+            counter += 1
+            if length <= half:
+                return counter
