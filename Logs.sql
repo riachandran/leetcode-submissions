715b920a42c0677aa147434caeabb0f61e7b4a13
Ria Thazhe Punathil <riachandrantp@gmail.com>
2026-06-11 16:41:15 -0700

    Add SQL query to find log ID range

---
+++ Logs.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT MIN(log_id) AS start_id, MAX(log_id) AS end_id FROM
+(
+    SELECT log_id, ROW_NUMBER() OVER (ORDER BY log_id) AS num FROM Logs
+) t
+GROUP BY log_id - num
