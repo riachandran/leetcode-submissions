4a977c09ef6ff0d2723c26a08ffadc37cadf0769
Ria Chandran Thazhe Punathil <riachandrantp@gmail.com>
2026-09-24 00:22:49 -0700

    Add SQL query to calculate click-through rate for ads

---
+++ ads.sql
@@ -0,0 +1,3 @@
+SELECT ad_id, ROUND(IFNULL(SUM(CASE WHEN action='Clicked' THEN 1 ELSE 0 END)*100/( SUM(CASE WHEN action='Clicked' THEN 1 ELSE 0 END) +  SUM(CASE WHEN action='Viewed' THEN 1 ELSE 0 END)),0),2) as ctr FROM Ads
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id ASC
